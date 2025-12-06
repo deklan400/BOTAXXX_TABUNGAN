e46fbe6b73154ce5ee86bfe6e21d811d531ad1ff
deklan400 <fadlan400@gmail.com>
2025-12-06 10:38:52 +0700

    Update savings.py

--- backend/app/models/savings.py
+++ backend/app/models/savings.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
 from sqlalchemy.orm import relationship
-
 from app.db.base import Base
 
 class Savings(Base):
@@ -8,6 +7,7 @@ class Savings(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
+
     date = Column(Date)
     type = Column(String)  # IN / OUT
     category = Column(String)
@@ -15,4 +15,3 @@ class Savings(Base):
     note = Column(String)
 
     user = relationship("User")
-
