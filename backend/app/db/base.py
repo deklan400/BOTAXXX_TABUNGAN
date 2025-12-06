d1df012f8b2a256b282f85eda4d92a96c653f98f
deklan400 <fadlan400@gmail.com>
2025-12-06 10:38:14 +0700

    Update base.py

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,13 +1,3 @@
-from app.models.user import User
-from app.models.savings import Savings
-from app.models.loan import Loan
-from app.models.target import Target
-from app.models.loan import LoanPayment
-
-from app.db.session import engine
-from app.db.session import SessionLocal
-from app.core.config import settings
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
-
